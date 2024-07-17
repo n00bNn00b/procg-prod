d510f87c3dab039f34029217cf9b911c20497aab
Jyanial Dewan <djyanial@gmail.com>
2024-07-18 02:13:43 +0600

    imported home.tsx to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from "./components/ui/button";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <>
       <Button className="text-2xl bg-slate-600">Click Me</Button>
+      <Home/>
     </>
   );
 }
