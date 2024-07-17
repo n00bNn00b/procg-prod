d717084c622278723cc0c094abe6638c012f7219
n00bNn00b <dewanreal@gmail.com>
2024-07-18 01:34:08 +0600

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="text-2xl bg-slate-600">Click Me</Button>
+    </>
+  );
+}
+
+export default App;
