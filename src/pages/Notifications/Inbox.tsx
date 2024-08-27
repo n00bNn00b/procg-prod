2308c2a006baa843dc381f64c2d6e40b57bff970
Jyanial Dewan <djyanial@gmail.com>
2024-08-27 20:40:07 +0600

    notification state management edited

--- src/pages/Notifications/Inbox.tsx
+++ src/pages/Notifications/Inbox.tsx
@@ -2,13 +2,20 @@ import { useGlobalContext } from "@/Context/GlobalContext/GlobalContext";
 import ComposeButton from "./ComposeButton";
 import NotificationCard from "./NotificationCard";
 import NotificationTable from "./NotificationTable";
+import { useEffect, useState } from "react";
+import { Message } from "@/types/interfaces/users.interface";
 
 const Inbox = () => {
   const { token, messages} = useGlobalContext();
-  
+  const [newMessages, setNewMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    setNewMessages(messages)
+  }, [messages]);
+
   const user = token.user_name;
-  const totalSentMessages = messages.filter(msg => msg.status === "Sent");
-  const totalDraftMessages = messages.filter(msg => msg.status === "Draft");
+  const totalSentMessages = newMessages.filter(msg => msg.status === "Sent");
+  const totalDraftMessages = newMessages.filter(msg => msg.status === "Draft");
   const recievedMessages = totalSentMessages.filter(msg => msg.recivers.includes(user));
   const sentMessages = totalSentMessages.filter(msg => msg.sender === user);
   const draftMessages = totalDraftMessages.filter(msg => msg.sender === user);
