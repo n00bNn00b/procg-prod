ce4da6b8a248f42c4a26ad51cfa049ed705442b6
nepolionchakma <nepolionchakma.nc@gmail.com>
2024-09-30 20:45:39 +0600

    Manage Access Model frontend add

--- src/pages/ManageAccessModels/ManageAccessModels.tsx
+++ src/pages/ManageAccessModels/ManageAccessModels.tsx
@@ -1,4 +1,56 @@
+import { ring } from "ldrs";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import SearchModels from "./SearchModels/SearchModels";
+import SearchResults from "./SearchResults/SearchResults";
+import { useState } from "react";
+
 const ManageAccessModels = () => {
-  return <div>ManageAccessModels</div>;
+  const [isSearchModelsOpen, setIsSearchModelsOpen] = useState(false);
+  const [isSearchResultsOpen, setIsSearchResultsOpen] = useState(false);
+  ring.register();
+  return (
+    <div className="bg-slate-100 p-2">
+      <div className="flex gap-2 items-center p-2">
+        {isSearchModelsOpen ? (
+          <ChevronUp
+            onClick={() => setIsSearchModelsOpen(!isSearchModelsOpen)}
+            className="border bg-white rounded cursor-pointer"
+          />
+        ) : (
+          <ChevronDown
+            onClick={() => setIsSearchModelsOpen(!isSearchModelsOpen)}
+            className="border bg-white rounded cursor-pointer"
+          />
+        )}
+
+        <h4>Serach Models</h4>
+      </div>
+      <div className="grid grid-cols-2">
+        {isSearchModelsOpen && <SearchModels />}
+
+        <div></div>
+      </div>
+      <div className="my-4">
+        <h4 className="font-semibold ml-2">Manage Models</h4>
+        <div className="p-2 border rounded-md bg-white">
+          <div className="flex gap-2 items-center my-1">
+            {isSearchModelsOpen ? (
+              <ChevronUp
+                onClick={() => setIsSearchResultsOpen(!isSearchResultsOpen)}
+                className="border bg-slate-100 rounded cursor-pointer"
+              />
+            ) : (
+              <ChevronDown
+                onClick={() => setIsSearchResultsOpen(!isSearchResultsOpen)}
+                className="border bg-slate-100 rounded cursor-pointer"
+              />
+            )}
+            <h4>Serach Results</h4>
+          </div>
+          {isSearchResultsOpen && <SearchResults />}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default ManageAccessModels;
