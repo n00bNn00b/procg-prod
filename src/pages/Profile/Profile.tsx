f3c660232a996b06ce071d9e661928a4e8681ff2
nepolionchakma <nepolionchakma.nc@gmail.com>
2024-12-02 18:07:45 +0600

    initiate QR Code

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -3,24 +3,52 @@ import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 import {
   Card,
   CardContent,
-  // CardDescription,
   CardFooter,
   CardHeader,
   // CardTitle,
 } from "@/components/ui/card";
+import { QRCodeCanvas } from "qrcode.react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { IUsersInfoTypes } from "@/types/interfaces/users.interface";
 import Account from "./Account";
 import { useEffect, useState } from "react";
-
+interface IAccessProfiles {
+  user_name?: string;
+  email?: string;
+  email_1?: string;
+  email_2?: string;
+  email_3?: string;
+  email_4?: string;
+  phone_1?: string;
+  phone_2?: string;
+  phone_3?: string;
+}
 const Profile = () => {
   const { token, getUserInfo } = useGlobalContext();
   const [userInfo, setUserInfo] = useState<IUsersInfoTypes | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [accessProfiles, setAccessProfiles] = useState<IAccessProfiles>({});
   useEffect(() => {
     const getUser = async () => {
-      const res = await getUserInfo(token?.user_id || 0);
-      if (res) {
-        setUserInfo(res);
+      try {
+        setIsLoading(true);
+        const res = await getUserInfo(token?.user_id || 0);
+
+        if (res) {
+          const accessProfiles: IAccessProfiles = {};
+          accessProfiles.user_name = res.user_name;
+          for (let i = 0; i < res?.email_addresses.length; i++) {
+            accessProfiles[`email_${i + 1}` as keyof IAccessProfiles] =
+              res.email_addresses[i];
+          }
+
+          setUserInfo(res);
+          setAccessProfiles(accessProfiles);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
       }
     };
     getUser();
@@ -39,17 +67,35 @@ const Profile = () => {
               Change your password here. After saving, you'll be logged out.
             </CardDescription> */}
           </CardHeader>
-          <CardContent className="space-y-2">
-            <Avatar>
-              <AvatarImage
-                className="object-cover object-center w-20 h-20 rounded-full mx-auto border border-8px"
-                src="https://github.com/shadcn.png"
-              />
-            </Avatar>
-            <h4 className="font-bold text-center">
-              {userInfo?.first_name} {userInfo?.last_name}
-            </h4>
-            <h4 className="text-center">Job Title : {userInfo?.job_title}</h4>
+          <CardContent>
+            {isLoading ? (
+              <div className="text-center">Loading</div>
+            ) : (
+              <div className="flex gap-10 justify-center">
+                <div className="flex flex-col items-center justify-center border p-4 rounded">
+                  <Avatar>
+                    <AvatarImage
+                      className="object-cover object-center w-20 h-20 rounded-full mx-auto border border-8px"
+                      src="https://github.com/shadcn.png"
+                    />
+                  </Avatar>
+                  <h4 className="font-bold text-center">
+                    {userInfo?.first_name} {userInfo?.last_name}
+                  </h4>
+                  <h4 className="text-center">
+                    Job Title : {userInfo?.job_title}
+                  </h4>
+                </div>
+                <div className="p-4 flex flex-col">
+                  <h1 className="font-bold ">Access Profiles</h1>
+                  <QRCodeCanvas
+                    value={JSON.stringify(accessProfiles)}
+                    title={"Access Profiles"}
+                    size={120}
+                  />
+                </div>
+              </div>
+            )}
           </CardContent>
           <CardFooter></CardFooter>
         </Card>
