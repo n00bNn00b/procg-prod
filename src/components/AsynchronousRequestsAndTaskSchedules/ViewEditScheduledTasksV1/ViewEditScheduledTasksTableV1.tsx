365e5cae541c7de9963e7a6e217be4a68841a508
nepolionchakma <nepolionchakma.nc@gmail.com>
2025-02-17 18:01:38 +0600

    cancel bug fix

--- src/components/AsynchronousRequestsAndTaskSchedules/ViewEditScheduledTasksV1/ViewEditScheduledTasksTableV1.tsx
+++ src/components/AsynchronousRequestsAndTaskSchedules/ViewEditScheduledTasksV1/ViewEditScheduledTasksTableV1.tsx
@@ -125,10 +125,6 @@ export function ViewEditScheduledTasksTableV1() {
       //table toggle empty
       table.getRowModel().rows.map((row) => row.toggleSelected(false));
       setSelected([]);
-      toast({
-        title: "Info !!!",
-        description: `Cancel successfully.`,
-      });
     } catch (error) {
       toast({
         title: "Info !!!",
@@ -194,7 +190,7 @@ export function ViewEditScheduledTasksTableV1() {
     //table toggle false
     table.toggleAllRowsSelected(false);
   };
-
+  console.log(selected, "selected");
   return (
     <div className="px-3">
       {isOpenModal === "edit_task_schedule_v1" && (
