59953d438d5da5a1701926253b46a19c6c707f7c
nepolionchakma <nepolionchakma.nc@gmail.com>
2025-03-13 15:37:44 +0600

    action item added

--- src/pages/Tools/EnterpriseProcessModeler/OrchestrationStudio2/Flow/components/CreateAFlow/CreateAFlow.tsx
+++ src/pages/Tools/EnterpriseProcessModeler/OrchestrationStudio2/Flow/components/CreateAFlow/CreateAFlow.tsx
@@ -2,7 +2,9 @@ import { toast } from "@/components/ui/use-toast";
 import useAxiosPrivate from "@/hooks/useAxiosPrivate";
 import { IOrchestrationDataTypes2 } from "@/types/interfaces/orchestration.interface";
 interface ICreateAFlowProps {
+  actionType: string;
   flowsData: IOrchestrationDataTypes2[];
+  selectedFlowData?: IOrchestrationDataTypes2 | undefined;
   newProcessName: string;
   setNewProcessName: React.Dispatch<React.SetStateAction<string>>;
   setCreateNewFlow: React.Dispatch<React.SetStateAction<boolean>>;
@@ -10,19 +12,23 @@ interface ICreateAFlowProps {
     React.SetStateAction<IOrchestrationDataTypes2 | undefined>
   >;
   setSelectedFlowName: React.Dispatch<React.SetStateAction<string>>;
+  setIsEditFlowName: React.Dispatch<React.SetStateAction<boolean>>;
   closeAllProgress: () => void;
 }
 const CreateAFlow = ({
+  actionType,
   flowsData,
+  selectedFlowData,
   newProcessName,
   setNewProcessName,
   setCreateNewFlow,
   setSelectedFlowData,
   setSelectedFlowName,
+  setIsEditFlowName,
   closeAllProgress,
 }: ICreateAFlowProps) => {
   const api = useAxiosPrivate();
-
+  console.log(actionType, "actionType");
   const handleCreateNewFlow = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
@@ -36,22 +42,49 @@ const CreateAFlow = ({
             description: "Flow already exists.",
           });
         } else {
+          const maxID = Math.max(...flowsData.map((flw) => flw.process_id));
           const postData = {
-            process_id: flowsData.length + 1,
+            process_id: maxID + 1,
             process_name: newProcessName,
             process_structure: { nodes: [], edges: [] },
           };
-          const res = await api.post("/orchestration-studio-process", postData);
-          if (res) {
-            setSelectedFlowName(newProcessName);
-            closeAllProgress();
-            setCreateNewFlow(false);
-            setSelectedFlowData(postData);
+          const putData = {
+            process_name: newProcessName,
+          };
 
-            toast({
-              title: "Success",
-              description: "New flow created successfully.",
-            });
+          if (actionType === "CreateAFlow") {
+            console.log(actionType, postData, "actionType");
+            const res = await api.post(
+              "/orchestration-studio-process",
+              postData
+            );
+            console.log(res, "res");
+            if (res) {
+              setSelectedFlowName(newProcessName);
+              closeAllProgress();
+              setCreateNewFlow(false);
+              setSelectedFlowData(postData);
+
+              toast({
+                title: "Success",
+                description: "New flow created successfully.",
+              });
+            }
+          } else if (actionType === "EditFlowName") {
+            console.log(actionType, putData, "actionType");
+            const res = await api.put(
+              `/orchestration-studio-process/process-name/${selectedFlowData?.process_id}`,
+              putData
+            );
+            console.log(res, "res");
+            if (res) {
+              setIsEditFlowName(false);
+              setSelectedFlowName(newProcessName);
+              toast({
+                title: "Success",
+                description: "Flow name updated successfully.",
+              });
+            }
           }
         }
       }
@@ -83,6 +116,7 @@ const CreateAFlow = ({
           onClick={(e) => {
             e.preventDefault();
             setCreateNewFlow(false);
+            setIsEditFlowName(false);
             setNewProcessName("");
           }}
         >
